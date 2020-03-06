cd9e8991039234a88356dabc6f56a3d5abd4a9a0
Gilbert <gssenyonjo@gmail.com>
2020-03-06 13:34:58 +0300

    Added comments for UserLG

--- src/Dyson.java
+++ src/Dyson.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Dyson {
     public static void main(String[] args){
         Scanner dyScanner = new Scanner(System.in);
-        int max = 0 ;
+        int max = 0;
 
         for (int i=0; i<5; i++){
             int input = dyScanner.nextInt();
--- src/UserLG.java
+++ src/UserLG.java
@@ -1,18 +1,36 @@
+/**
+ * 27th Feb 2020
+ * By Dyson, under supervision by GilbertS
+ * In this file Dyson learns how to use the Scanner class to
+ * request multiple inputs from the command line one at a time
+ * She uses the inputs to calculate and print their total
+ */
+
+// We import the Scanner class from the util library
 import java.util.Scanner;
 
+// Create a class called UserLG
 public class UserLG {
     public static void main(String args[]){
+        // We test to see if everything is fine by running a simple print command
         System.out.println("hello Dyson");
 
+        // We create a new Scanner object.
+        // This object will accept inputs from the keyboard
         Scanner myScanner = new Scanner(System.in);
 
+        // we call nextInt on our scanner object which returns an integer
+        // we store the integer in an integer variable (eg num1, num2 ... )
         int num1 = myScanner.nextInt();
         int num2 = myScanner.nextInt();
         int num3 = myScanner.nextInt();
         int num4 = myScanner.nextInt();
         int num5 = myScanner.nextInt();
 
-        int total = num1 + num2+ num3+num4+ num5;
+        // We compute the total using addition arithmetic
+        int total = num1 + num2 + num3 + num4 + num5;
+
+        // Finally, we print the total
         System.out.println("the total is " + total);
     }
 }
